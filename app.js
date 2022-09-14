8254ca5540cfa75c30d12220f6954c07160a4761
Casey Hudler <chudler624@gmail.com>
2022-09-14 15:12:39 -0400

    complete

--- app.js
+++ app.js
@@ -4,7 +4,39 @@ console.log(
 );
 
 // Exercise 1
+const firstName = "Casey";
+let lastName = "Hudler";
+var age= 33;
 
 // Exercise 2
+Casey = string;
+Hudler = string;
+33 = number;
 
 // Exercise 3
+let language = "JavaScript";
+let createdYear = 1995;
+let isCaseSensitive = true;
+
+//Exercise 4
+let price;
+price = 19.99;
+
+let isOnSale;
+isOnSale = false;
+
+let salePercentage;
+salePercentage = 15;
+
+let inStock;
+stock = 0; 
+
+let selectedSize;
+selectedSize = "M";
+
+//Exercise 5
+let title = "Name of the wind";
+let author = "Patrick Rothfuss";
+let pageCount = 722;
+let bookmark = 456;
+let hasRead = true;
